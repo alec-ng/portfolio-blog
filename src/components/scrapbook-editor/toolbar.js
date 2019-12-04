a9c2f7d32df68c75e920dff05bbb9b702a7b15a4
Alec N <alecng94@gmail.com>
2019-12-04 17:50:53 +0700

    add react-bootstrap

--- src/components/scrapbook-editor/toolbar.js
+++ src/components/scrapbook-editor/toolbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import useStateValue from "./state";
 import PageMetadata from "./page-metadata";
-
+import Accordion from "./components/accordion";
 /**
  * Represents the editor's command bar for modifying page and block metadata
  */
@@ -9,7 +9,10 @@ export default function Toolbar(props) {
   return (
     <div style={{ height: "100%", border: "1px solid black" }}>
       <h1>This is my toolbar.</h1>
-      <PageMetadata />
+      <Accordion title="Page Metadata">
+        <PageMetadata />
+      </Accordion>
+      <hr />
     </div>
   );
 }
