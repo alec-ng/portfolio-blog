64f3b28937778cb2e8078b8446b1b6afb3444542
Alec N <alecng94@gmail.com>
2019-12-24 11:02:04 -0800

    code refactoring

---
+++ src/components/scrapbook-editor/components/block-attribute-group.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Input from "./input";
+
+/**
+ * Represents a grouping of inputs to be rendered in BlockAttributes in the Toolbar
+ * The groupings are either "base" or some variation
+ */
+export default function BlockAttributeGroup(props) {
+  let attrList = [];
+  props.attrs.forEach(attr => {
+    let inputAttrs = {
+      "data-name": attr.name
+    };
+    if (props.isBase) {
+      inputAttrs["data-variation"] = "base";
+      inputAttrs["value"] = props.focusedBlock.baseAttrs[attr.name] || "";
+      inputAttrs["checked"] = props.focusedBlock.baseAttrs[attr.name] === true;
+    } else {
+      inputAttrs["data-variation"] = props.variationName;
+      inputAttrs["value"] =
+        props.focusedBlock.variationAttrs[props.variationName][attr.name] || "";
+      inputAttrs["checked"] =
+        props.focusedBlock.variationAttrs[props.variationName][attr.name] ===
+        true;
+    }
+
+    attrList.push(
+      <Input
+        label={attr.label}
+        type={attr.type}
+        handleOnInput={props.onInput}
+        key={attr.name}
+        attributes={inputAttrs}
+      />
+    );
+  });
+  return (
+    <>
+      {props.isBase
+        ? attrList
+        : attrList.length > 0 && (
+            <>
+              {" "}
+              <hr /> {attrList}{" "}
+            </>
+          )}
+    </>
+  );
+}
--- src/components/scrapbook-editor/components/block-attributes.js
+++ src/components/scrapbook-editor/components/block-attributes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useStateValue } from "./../state";
 import { ACTION_TYPES } from "./../reducers/index";
 import Input from "./input";
+import AttributeGroup from "./block-attribute-group";
 
 /**
  * Set of inputs that matches the attributes of the focused block on the canvas
@@ -21,17 +22,20 @@ export default function BlockAttributes(props) {
   }
 
   // Selected block has defined its own controls
-  if (focusedBlock.useDefaultControls) {
-    // TODO: implement with doing custom controls
-    return <h1>TODO!</h1>;
+  const plugin = pluginMap[focusedBlock.name];
+  if (!plugin.useDefaultControls) {
+    return <h1>//TODO: Rendering and value updating!</h1>;
   }
 
   // Selected block relies on auto generated controls
   const onInput = function(e) {
-    // TODO: handle checkbox and select cases
-    // TODO: for numbers, cast from string
-    let newVal =
-      e.target.type === "checkbox" ? e.target.checked : e.target.value;
+    let newVal = e.target.value;
+    if (e.target.type === "checkbox") {
+      newVal = e.target.checked;
+    }
+    if (e.target.type === "number") {
+      newVal = parseFloat(e.target.value);
+    }
     dispatch({
       type: ACTION_TYPES.UPDATE_FOCUSED_BLOCK,
       payload: {
@@ -42,61 +46,28 @@ export default function BlockAttributes(props) {
     });
   };
 
-  const plugin = pluginMap[focusedBlock.name];
-
-  // Render base attributes
-  let baseAttrList = [];
-  plugin.baseAttrs.forEach(attr => {
-    let inputAttrs = {
-      "data-name": attr.name,
-      "data-variation": "base",
-      value: focusedBlock.baseAttrs[attr.name] || "",
-      checked: focusedBlock.baseAttrs[attr.name] === true
-    };
-    baseAttrList.push(
-      <Input
-        label={attr.label}
-        type={attr.type}
-        handleOnInput={onInput}
-        key={attr.name}
-        attributes={inputAttrs}
-      />
-    );
-  });
-
-  // Render variation attributes
-  let variationAttrList = [];
+  let baseAttrProps = {
+    attrs: plugin.baseAttrs,
+    isBase: true,
+    onInput: onInput,
+    focusedBlock: focusedBlock
+  };
   let variationName = focusedBlock.variation;
   let variation = plugin.variations.find(
     variation => variation.name === variationName
   );
-  variation.attrs.forEach(attr => {
-    let inputAttrs = {
-      "data-name": attr.name,
-      "data-variation": variationName,
-      value: focusedBlock.variationAttrs[variationName][attr.name] || "",
-      checked: focusedBlock.variationAttrs[variationName][attr.name] === true
-    };
-    variationAttrList.push(
-      <Input
-        label={attr.label}
-        type={attr.type}
-        handleOnInput={onInput}
-        attributes={inputAttrs}
-        key={attr.name}
-      />
-    );
-  });
+  let variationAttrProps = {
+    attrs: variation.attrs,
+    isBase: false,
+    onInput: onInput,
+    variationName: variationName,
+    focusedBlock: focusedBlock
+  };
 
   return (
     <form>
-      {baseAttrList}
-      {variationAttrList.length > 0 && (
-        <>
-          <hr />
-          {variationAttrList}
-        </>
-      )}
+      <AttributeGroup {...baseAttrProps} />
+      <AttributeGroup {...variationAttrProps} />
     </form>
   );
 }
