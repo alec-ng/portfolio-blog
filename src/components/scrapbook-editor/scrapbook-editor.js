2541d43195071cc77c9b1a7e450f54b1dd3e2e9f
Alec N <alecng94@gmail.com>
2019-12-25 15:53:25 -0800

    Revert back app.js child -- somehow breaking render

--- src/components/scrapbook-editor/scrapbook-editor.js
+++ src/components/scrapbook-editor/scrapbook-editor.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { StateProvider, DefaultState } from "./state";
 import { MainReducer } from "./reducers/index";
-import App from "./app";
+
+import styled from "styled-components";
+import Toolbar from "./toolbar";
+import Canvas from "./canvas";
+import PreviewButton from "./components/preview-button";
 
 /**
  * Inits an object to be used as the pageData prop for ScrapbookEditor
@@ -60,9 +64,35 @@ export function ScrapbookEditor(props) {
   // Add save cb to global state
   globalState.onSave = props.onSave;
 
+  const BaseContainer = styled.div`
+    margin-left: auto;
+    margin-right: auto;
+    display: flex;
+    height: 100%;
+    min-width: ${props => (props.readOnly ? "inherit" : "992px")};
+  `;
+  const ToolbarContainer = styled.div`
+    flex: 0 0 25%;
+    overflow-y: auto;
+  `;
+  const CanvasContainer = styled.div`
+    overflow-y: auto;
+    flex: ${props => (props.readOnly ? "100%" : "75%")};
+  `;
+
   return (
     <StateProvider initialState={globalState} reducer={MainReducer}>
-      <App />
+      <BaseContainer readOnly={globalState.readOnly}>
+        {!globalState.readOnly && !globalState.inPreviewMode && (
+          <ToolbarContainer>
+            <Toolbar />
+          </ToolbarContainer>
+        )}
+        <CanvasContainer readOnly={globalState.readOnly}>
+          <Canvas />
+        </CanvasContainer>
+        {globalState.inPreviewMode && <PreviewButton />}
+      </BaseContainer>
     </StateProvider>
   );
 }
