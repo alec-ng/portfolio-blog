4b820e4c0bbe449692b772184d70859326cf3cdd
Alec N <alecng94@gmail.com>
2020-01-24 15:22:38 -0800

    add sandbox/prod configs and scripts

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -2,7 +2,7 @@ import app from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-const config = {
+const prodConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
@@ -12,6 +12,19 @@ const config = {
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
+
+const sbxConfig = {
+  apiKey: process.env.REACT_APP_SBX_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_SBX_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_SBX_DATABASE_URL,
+  projectId: process.env.REACT_APP_SBX_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_SBX_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SBX_MESSENGER_SENDER_ID,
+  appId: process.env.REACT_APP_SBX_APP_ID
+};
+
+const config =
+  process.env.REACT_APP_ENV === "production" ? prodConfig : sbxConfig;
 
 const COLLECTION_CMS_POST = "cms-post";
 const COLLECTION_POST = "post";
