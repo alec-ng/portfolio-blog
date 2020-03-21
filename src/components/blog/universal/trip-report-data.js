1faa28cf45d01c97b7a741cf83444e36168b2fe1
Alec N <alecng94@gmail.com>
2020-03-21 11:06:46 -0700

    remove hyperlink

--- src/components/blog/universal/trip-report-data.js
+++ src/components/blog/universal/trip-report-data.js
@@ -1,38 +1,25 @@
 import React from "react";
-import { useLocation, useHistory } from "react-router-dom";
-import { urlEncodeStr } from "../../../util/url-util";
-
 import PublicOutlinedIcon from "@material-ui/icons/PublicOutlined";
 import MapOutlinedIcon from "@material-ui/icons/MapOutlined";
 
 /**
- * Shows a trip report's custom metadata
- * onClick, initiate a state push to update filters
+ * Renderes readonly trip report metdata
  */
 export default function TripReportData({ postMetadata }) {
-  const pathname = useLocation().pathname;
-  const history = useHistory();
-  const urlRegion = urlEncodeStr(postMetadata.region);
-  const urlArea = urlEncodeStr(postMetadata.area);
-
-  // programmatic navigation to preserve SPA UX
-  function onClick(e) {
-    e.preventDefault();
-    history.push(e.currentTarget.dataset.href);
-  }
-
-  const regionUrl = `${pathname}?region=${urlRegion}`;
-  const areaUrl = `${pathname}?region=${urlRegion}&area=${urlArea}`;
-
   return (
     <div className="brandywine-width_large mx-auto mb-4">
-      <a onClick={onClick} href="#" data-href={regionUrl}>
-        <PublicOutlinedIcon /> {postMetadata.region}
-      </a>
-      <br />
-      <a onClick={onClick} href="#" data-href={areaUrl}>
-        <MapOutlinedIcon /> {postMetadata.area}
-      </a>
+      <div>
+        <span style={{ color: "rgb(255, 69, 0)" }}>
+          <PublicOutlinedIcon />
+        </span>
+        &nbsp;{postMetadata.region}
+      </div>
+      <div>
+        <span style={{ color: "rgb(255, 69, 0)" }}>
+          <MapOutlinedIcon />
+        </span>
+        &nbsp;{postMetadata.area}
+      </div>
     </div>
   );
 }
